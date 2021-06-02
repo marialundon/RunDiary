bbf4716730c0ab90f94df08fada79107e17257ff
marialundon <marialundon@gmail.com>
2021-06-02 08:03:20 +0100

    First go at using SQLAlchemy

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-  
-@app.route("/")
-def home():
-   return render_template("home.html")
+from flask import Flask, render_template 
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route("/Maria")
-def maria():
-   return 'Hello Maria'
-  
-if __name__ == '__main__':
-   app.run(debug=True)
+app = Flask (__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///runs.sqlite3'
+
+db = SQLAlchemy(app)
+class runs(db.Model):
+   id = db.Column('run_id', db.Integer, primary_key = True)
+   typeofrun = db.Column(db.String(100))
+   length = db.Column(db.String(50))  
+   location = db.Column(db.String(200))
+   time = db.Column(db.String(10))
+
+def __init__(self, typeofrun, length, location,time):
+   self.typeofrun = typeofrun
+   self.length = length
+   self.location = location
+   self.time = time
+
+db.create_all()
