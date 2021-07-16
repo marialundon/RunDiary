29744bea549aa909e36e7a376a071cd9dca57d45
marialundon <marialundon@gmail.com>
2021-07-16 14:40:50 +0100

    Shoes column added to runs table

--- Controllers/RunController.py
+++ Controllers/RunController.py
@@ -2,6 +2,7 @@
 from Model.Run import Run
 from Model.Runtype import Runtype
 from Model.Runlocation import Runlocation
+from Model.Shoe import Shoe
 from flask import Blueprint, request, render_template, flash, url_for, redirect
 from Model import db
 from sqlalchemy import desc
@@ -21,17 +22,17 @@ def run_options():
 @new_run.route("/runs/new", methods=["GET","POST"])
 def run_new():
     if request.method == 'POST':
-        if not request.form.get('typeofrun','') or not request.form.get('date','') or not request.form.get('length','') or not request.form.get('time','') or not request.form.get('location',''):
+        if not request.form.get('typeofrun','') or not request.form.get('date','') or not request.form.get('shoe','') or not request.form.get('length','') or not request.form.get('time','') or not request.form.get('location',''):
             flash('Please enter all the fields', 'error')
         else:
-            run = Run(request.form.get('typeofrun'),request.form.get('date'),request.form.get('length'),request.form.get('time'), request.form.get('location'))
+            run = Run(request.form.get('typeofrun'),request.form.get('date'),request.form.get('length'),request.form.get('time'), request.form.get('location'),request.form.get('shoe'))
             
             db.session.add(run)
             db.session.commit()
             print('Hello')
             flash('Record was successfully added')
             return redirect(url_for('options_run.run_options'))
-    return render_template('run_new.html', Runtype = Runtype.query.all(),Runlocation = Runlocation.query.all())
+    return render_template('run_new.html', Runtype = Runtype.query.all(),Runlocation = Runlocation.query.all(),Shoe = Shoe.query.all())
     
 @data_run.route('/runs/data')
 def run_data():
@@ -57,9 +58,10 @@ def run_update(id):
             run.length = request.form.get('length')
             run.time = request.form.get('time')
             run.location = request.form.get('location')
+            run.shoe = request.form.get('shoe')
 
             db.session.commit()
             flash('Record was successfully updated')
             return redirect(url_for('options_run.run_options'))
-    return render_template('run_update.html',Runtype = Runtype.query.all(), Runlocation = Runlocation.query.all(),runtobeupdated=run)
+    return render_template('run_update.html',Runtype = Runtype.query.all(), Runlocation = Runlocation.query.all(),runtobeupdated=run,Shoe = Shoe.query.all())
 
