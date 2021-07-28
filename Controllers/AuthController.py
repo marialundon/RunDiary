f46b3b90f812b770fac747262c506f21afaf0cf4
marialundon <marialundon@gmail.com>
2021-07-28 16:20:21 +0100

    Working on log in

--- Controllers/AuthController.py
+++ Controllers/AuthController.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from Model import db
-from flask import Blueprint, request, render_template, flash, url_for, redirect
+from flask import Blueprint, request, render_template, url_for, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from Model.User import User
 
 auth = Blueprint('auth', __name__)
 
@@ -9,8 +11,29 @@ def login():
     return render_template('login.html')
 
 @auth.route('/signup')
+
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user: 
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(email=email, name=name, password=password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
---
+++ Model/User.py
@@ -0,0 +1,21 @@
+from flask.app import Flask
+from Model import db
+
+app = Flask('__main__')
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///runs.sqlite3'
+app.config['SECRET_KEY'] = "123456789"
+
+
+db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+def __init__(self, email, password, name):
+    self.email = email
+    self.password = password
+    self.name = name
+        
