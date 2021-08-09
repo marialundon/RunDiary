2bedef2ec2f8bbaf93ea66abeb443d633b342b7e
marialundon <marialundon@gmail.com>
2021-08-09 15:24:54 +0100

    another apth change

--- Controllers/AuthController.py
+++ Controllers/AuthController.py
@@ -11,7 +11,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('~/login.html')
 
 @auth.route('/signup')
 def signup():
